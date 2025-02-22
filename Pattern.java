91bc8f5dba76e27bafe3aaf161af378e51472ecf
Chandana B <chandanab83.04@gmail.com>
2025-02-22 20:56:41 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,48 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        //------------------------------------for Upper Part----------------------
+        for (int i = 1; i <= 5; i++) 
+        {
+            for (int j = 1; j <=i; j++) 
+            {
+                System.out.print(" *");
+            }
+            for (int j = 4; j>=i; j--) 
+            {
+                System.out.print("  ");
+            }
+            for (int j = 4; j>=i; j--) 
+            {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <=i; j++) 
+            {
+                System.out.print(" *");
+            }
+                    System.out.println( );
+        }
+
+        //---------------------------------------------Lower part--------------------------------------------------
+        for (int i = 1; i <= 5; i++) 
+        {
+            for (int j = 5; j>=i; j--) 
+            {
+                System.out.print(" *");
+            }
+            for (int j = 1; j<=i-1; j++) 
+            {
+                System.out.print("  ");
+            }
+            for (int j = 1; j<=i-1; j++) 
+            {
+                System.out.print("  ");
+            }
+            for (int j = 5; j >=i; j--) 
+            {
+                System.out.print(" *");
+            }
+                    System.out.println( );
+        }
+    }
+}
